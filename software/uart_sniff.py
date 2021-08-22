182499e5a4f5366b282674926470386f7033c3d2
James Chambers <jameschambers2@gmail.com>
2021-08-22 17:18:18 -0400

    separate UART sync receive function

--- software/uart_sniff.py
+++ software/uart_sniff.py
@@ -2,6 +2,13 @@
 import argparse
 from pylibftdi import (Device, INTERFACE_B)
 
+# Recognized JoyBus commands
+CMD_INFO_RESET = 0xff
+CMD_INFO = 0x00
+CMD_STATE = 0x01
+CMD_PAK_READ = 0x02
+CMD_PAK_WRITE = 0x03
+
 
 def sendall(ser, data):
     n = 0
@@ -15,6 +22,48 @@ def extract_addr(packed):
     address *= 32
     crc = packed[1] & 0x1f
     return address, crc
+
+
+def sync_recv(ser, verbose=False):
+    while True:
+        # get number of bytes to receive
+        sync_magic1 = ser.read(1)
+        if len(sync_magic1) != 1:
+            continue
+        if sync_magic1 != b'\xaa':
+            if verbose:
+                print('out of sync')
+                print(sync_magic1)
+            continue
+
+        sync_magic2 = ser.read(1)
+        if len(sync_magic2) != 1 or sync_magic2 != b'\x55':
+            if verbose:
+                print('out of sync 2')
+                print(sync_magic2)
+            continue
+
+        byte_count = ser.read(1)
+        if len(byte_count) != 1:
+            print('error: no command length received')
+        byte_count = ord(byte_count)
+
+        response_byte_count = ser.read(1)
+        if len(response_byte_count) != 1:
+            print('error: no response length received')
+            continue
+        response_byte_count = ord(response_byte_count)
+
+        bytez = ser.read(byte_count)
+        if len(bytez) == 0:
+            continue
+
+        if response_byte_count > 0:
+            response_bytez = ser.read(response_byte_count)
+        else:
+            response_bytez = b''
+
+        return (bytez, response_bytez)
 
 
 def main():
@@ -34,58 +83,21 @@ def main():
         ser.flush_input()
 
         while True:
-            # get number of bytes to receive
-            sync_magic1 = ser.read(1)
-            if len(sync_magic1) != 1:
-                continue
-            if sync_magic1 != b'\xaa':
-                if args.verbose:
-                    print('out of sync')
-                    print(sync_magic1)
-                continue
-
-            sync_magic2 = ser.read(1)
-            if len(sync_magic2) != 1 or sync_magic2 != b'\x55':
-                if args.verbose:
-                    print('out of sync 2')
-                    print(sync_magic2)
-                continue
-
-            byte_count = ser.read(1)
-            if len(byte_count) != 1:
-                print('error: no command length received')
-            byte_count = ord(byte_count)
-
-            response_byte_count = ser.read(1)
-            if len(response_byte_count) != 1:
-                print('error: no response length received')
-                continue
-            response_byte_count = ord(response_byte_count)
-
-            bytez = ser.read(byte_count)
-            if len(bytez) == 0:
-                continue
-
-            if response_byte_count > 0:
-                response_bytez = ser.read(response_byte_count)
-            else:
-                response_bytez = None
-
-            if args.verbose:
-                print(f'rcving {byte_count} command bytes, {response_byte_count} response bytes')
+            bytez, response_bytez = sync_recv(ser, args.verbose)
 
             cmd = bytez[0]
-            if args.verbose and cmd != 1:
+            if args.verbose and cmd != CMD_STATE:
                 print(f'cmd {cmd:02x}')
                 print(bytez.hex())
 
-            if cmd in [0, 1, 0xff]:
+            if cmd in [CMD_INFO, CMD_INFO_RESET, CMD_STATE]:
+                # state cmd is spammy
                 pass
-            elif cmd == 2:
+            elif cmd == CMD_PAK_READ:
                 address, crc = extract_addr(bytez[1:3])
                 print(f'read cmd: {address:04x} (addr CRC-5 {crc:02x})')
                 print(f'  response: {response_bytez.hex()}')
-            elif cmd == 3:
+            elif cmd == CMD_PAK_WRITE:
                 address, crc = extract_addr(bytez[1:3])
                 data = bytez[3:]
                 print(f'write cmd: {address:04x} (addr CRC-5 {crc:02x})')
